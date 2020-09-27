c60ce836d0b19d1deba26a87f9014fd76df76f2b
Ashraj Grewal <ashraj.grewal10@gmail.com>
2020-09-27 14:24:00 -0700

    Create Chatbot.py

---
+++ Chatbot.py
@@ -0,0 +1,16 @@
+#chatbot Ashraj Grewal cs1.0 
+def get_bot_response(user_response)
+bot_response_happy = ["Great! That's how you belong", "Keep the good times going!", "Good, life is too short to be anything else"]
+bot_response_angry = ["Sorry to hear that, take some time to think", "Have a cup of tea!", "Go listen to some calming music"]
+bot_response_sad =  ["It is okay to not be okay!", "We need to have downs to appreciate the ups", "*Virtual Hug*"]
+bot_response_okay = ["being confused is natural", "use this time to think", "Do something you love to center yourself"]
+if user_response == "happy":
+    return choice(bot_response_happy)
+  elif user_response == "angry":
+    return choice(bot_response_sad)
+    elif user_response == "sad":
+        return choice(bot_response_sad)
+    elif user_response == "okay":
+        return choice(bot_response_okay)
+  else:
+    return "We need to feel all emotions to stay human :)"
